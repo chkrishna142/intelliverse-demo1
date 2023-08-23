fb8952505bfccaed42900594b922a06070b10185
Salil Upadhyay <upsalil.01@gmail.com>
2023-08-23 18:29:40 +0530

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { Box } from '@chakra-ui/layout';
-import './App.css';
-import Navbar from './components/Navbar';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Main/Home';
-import Sizing from "./components/Sizing/Sizing"
-import Login from './components/Auth/Login';
-import { get_auth_status } from './redux/Auth/auth.selectors';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import Sidebar from './components/Sidebar/Sidebar';
-import AiAdvisor from './components/Main/AIAdvisor';
-import NavContext from './components/NavContext';
-import { useState } from 'react';
-import BF_Dashboard from './components/BlastFurnace/BF_Components/BF_Dashboard';
+import { Box } from "@chakra-ui/layout";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Main/Home";
+import Sizing from "./components/Sizing/Sizing";
+import Login from "./components/Auth/Login";
+import { get_auth_status } from "./redux/Auth/auth.selectors";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import AiAdvisor from "./components/Main/AIAdvisor";
+import NavContext from "./components/NavContext";
+import { useState } from "react";
+import BF_Dashboard from "./components/BlastFurnace/BF_Components/BF_Dashboard";
+import ConatctUs from "./components/ContactUs/ConatctUs";
 
 function App() {
   const { isAuth, default_plant } = useSelector(get_auth_status);
-  const [login, setLogin] = useState(false)
+  const [login, setLogin] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("logged_in")) {
-      setLogin(true)
+      setLogin(true);
     } else {
-      setLogin(false)
+      setLogin(false);
     }
-  }, [])
+  }, []);
 
- 
   return (
     <>
-      <NavContext.Provider value={{ setLogin}}>
+      <NavContext.Provider value={{ setLogin }}>
         <Box
           maxW="100vw"
           boxSizing="border-box"
@@ -41,27 +41,33 @@ function App() {
             <>
               <Navbar />
               <Sidebar />
-              <div className="overall_container" style={{ display: 'flex' }}>
+              <div className="overall_container" style={{ display: "flex" }}>
                 <div
                   className="routes_container"
-                  style={{ width: '100%', marginTop: '70px', marginLeft:'130px', marginRight:'40px' }}
+                  style={{
+                    width: "100%",
+                    marginTop: "70px",
+                    marginLeft: "130px",
+                    marginRight: "40px",
+                  }}
                 >
                   <Routes>
                     {default_plant?.length > 0 ? (
                       <Route
                         path="/"
-                        element={
-                          <Navigate to={`/intelliverse`} />
-                        }
+                        element={<Navigate to={`/intelliverse`} />}
                       />
                     ) : (
-                      <Route path="/" element={<Navigate to="/intelliverse" />} />
-                    )}       
+                      <Route
+                        path="/"
+                        element={<Navigate to="/intelliverse" />}
+                      />
+                    )}
                     <Route path="/intelliverse" element={<Home />} />
-                    <Route path="/advisor" element={<AiAdvisor />} /> 
-                    <Route path="/blastfurnace" element={<BF_Dashboard />} />  
-                    <Route path="/intelliverse/Sizing" element={<Sizing/>} />
-
+                    <Route path="/advisor" element={<AiAdvisor />} />
+                    <Route path="/blastfurnace" element={<BF_Dashboard />} />
+                    <Route path="/intelliverse/Sizing" element={<Sizing />} />
+                    <Route path="/contactus" element={<ConatctUs />} />
                   </Routes>
                 </div>
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
     screens: {
-      'xs': '370px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      '3xl': '1920px',
+      xs: "370px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
     },
   },
 };
-
