3325a8a377f721af971ecfe5d5733c716c62fc3f
Ahaan <ahaan@ripik.in>
2023-09-04 23:37:02 +0530

    Minor fix

--- src/components/Main/NewUseCaseModal.jsx
+++ src/components/Main/NewUseCaseModal.jsx
@@ -30,13 +30,13 @@ const NewUseCaseModal = ({ isOpen, onOpen, onClose }) => {
                     <ModalBody>
                         {submitted === false ? <div><div className='mt-2'>
                             <p className='text-[#034D86] font-semibold '>Title</p>
-                            <textarea placeholder='Be concise and include the most important requirement' className='h-12 w-full border rounded-md mt-3 px-2 py-2 text-sm' />
+                            <textarea placeholder='Be concise and include the most important requirement' className='h-18 w-full border rounded-md mt-3 px-2 py-2 text-sm' />
 
                         </div>
                             <div className='mt-2'>
                                 <p className='text-[#034D86] font-semibold '>Details</p>
                                 <p className='font-light text-sm mt-2'>Provide a clear and concise description of the new use case. Kindly include information about its objectives, intended benefit, and usage. Feel free to elaborate on any specific requirements or unique aspects of the use case that you believe are important for us to consider. The more information you provide, the better we can assess the feasibility and potential impact of adding this use case to our plattorm.</p>
-                                <textarea placeholder='Enter the details' className='h-12 w-full border rounded-md mt-3 px-2 py-2 text-sm' />
+                                <textarea placeholder='Enter the details' className='h-20 w-full border rounded-md mt-3 px-2 py-2 text-sm' />
                             </div>
                             <div className='grid grid-cols-2 gap-6'>
                                 <div className='mt-2'>
