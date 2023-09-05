1cc5293b16e7fee62a5d9ebe2341415a78f1bc77
Ahaan <ahaan@ripik.in>
2023-09-05 17:35:08 +0530

    Mobile updates

--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -26,8 +26,8 @@ const Home = ({ state }) => {
                         <div onClick={() => setIsOpen(true)} className=' md:flex flex items-center gap-3 font-bold text-white bg-[#3182CE] md:mt-3 px-2 py-2 h-8 border rounded-md md:text-xs cursor-pointer hover:bg-[#024D87] hover:transition duration-200 text-[10px]  '><div className='ml-2 '>+</div><div className='mr-3 '>Add Use Case</div></div>
                     </div>
 
-                    <div className=' w-full h-full ml-3 mb-5'>
-                        <div className='mt-10 md:mt-5 flex flex-wrap gap-8 text-sm items-center'>
+                    <div className=' w-full h-full mb-5'>
+                        <div className='mt-10 md:mt-5 ml-0 md:ml-3 flex flex-wrap md:justify-start justify-center gap-8 text-sm items-center'>
                             <Link to="/vision/Sizing" style={{ textDecoration: 'none' }}>
                                 <div>
                                     <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
@@ -69,6 +69,14 @@ const Home = ({ state }) => {
                                 </div>
                                 <div className='mt-4 flex justify-center h-10 w-28'><p className='font-bold text-[#024D87] text-center'>Automate Data Digitization </p></div>
                             </div>
+                            <div className='invisible'>
+                                <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
+                                    <div className='w-full flex justify-center '><img className='mt-4 h-20 w-20 p-3' src="/ocr.svg" /></div>
+                                    <div className='w-full flex justify-center text-[#024D87]  text-xs'><div className='bg-[#CCEAFF] px-2 py-1 w-full font-bold text-xs'>2 Deployments</div></div>
+                                </div>
+                                <div className='mt-4 flex justify-center h-10 w-28'><p className='font-bold text-[#024D87] text-center'>Automate Data Digitization </p></div>
+                            </div>  
+                            
 
                         </div>
                     </div>
@@ -82,7 +90,7 @@ const Home = ({ state }) => {
                         <div className='grid grid-cols-2 gap-2 md:mt-0 mt-7'>
                             <div className='mt-2 border-r'>
                                 <div className='w-full flex justify-start ml-3 text-gray-500 font-bold'>Scheduling</div>
-                                <div className='mt-8 flex flex-wrap gap-8 mb-3 ml-3  items-center '>
+                                <div className='mt-8 flex flex-wrap gap-8 mb-3 md:ml-3 ml-0 md:justify-start justify-center  items-center '>
                                     <div>
                                         <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
                                             <div className='flex justify-end -mt-5'><div className="h-8 w-8 rounded-full bg-orange-500 flex justify-center items-center text-lg text-white">2</div></div>
@@ -142,11 +150,11 @@ const Home = ({ state }) => {
                     <div className='border shadow-md mt-4 px-4 py-4 pb-20 md:pb-5 pl-5 rounded-md text-sm mt-6 bg-white'>
                         <div className='mt-0 mr-3 -ml-0 flex justify-between '>
                             <img className='lg:h-14 h-10 ml-2' src="/community.svg" />
-                            <div onClick={() => setIsOpen(true)} className=' md:flex flex h-8 items-center gap-3 font-bold text-white bg-[#3182CE] md:mt-3 px-2 py-2  border rounded-md md:text-xs cursor-pointer hover:bg-[#024D87] hover:transition duration-200 text-[10px]  '><div className='ml-2 '>+</div><div className='mr-3 '>Add Use Case</div></div>
+                            <div onClick={() => setIsOpen(true)} className=' md:flex md:mt-0 mt-3 flex h-8 items-center gap-3 font-bold text-white bg-[#3182CE] md:mt-3 px-2 py-2  border rounded-md md:text-xs cursor-pointer hover:bg-[#024D87] hover:transition duration-200 text-[10px]  '><div className='ml-2 '>+</div><div className='mr-3 '>Add Use Case</div></div>
                         </div>
                         <div>
-                            <div className='mt-0'>
-                                <div className='mt-4 flex flex-wrap gap-8 mb-3 ml-3  items-center '>
+                            <div className='md:mt-0 mt-10'>
+                                <div className='mt-4 flex flex-wrap gap-8 mb-3 md:ml-3 ml-0 md:justify-start justify-center items-center '>
                                     <Link to="/community/askanexpert">
                                         <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
                                             {/* <div className='flex justify-end -mt-5'><div className="h-8 w-8 rounded-full bg-orange-500 flex justify-center items-center text-lg text-white">2</div></div> */}
@@ -163,6 +171,14 @@ const Home = ({ state }) => {
                                         <div className='mt-4 flex justify-center h-10 w-28'><p className='font-bold text-[#024D87] text-center'>AI Advisor </p></div>
                                     </Link>
                                     <div>
+                                        <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
+                                            {/* <div className='flex justify-end -mt-5'><div className="h-8 w-8 rounded-full bg-orange-500 flex justify-center items-center text-lg text-white">1</div></div> */}
+                                            <div className='w-full flex justify-center items-center '><img className='object-fit' src="/people.svg" /></div>
+                                            {/* <div className='w-full flex justify-center text-[#024D87]  text-xs'><div className='bg-[#CCEAFF] px-2 py-1 w-full font-bold text-xs'>5 Deployments</div></div> */}
+                                        </div>
+                                        <div className='mt-4 flex justify-center h-10 w-28'><p className='font-bold text-center text-[#024D87]'>Community</p></div>
+                                    </div>
+                                    <div className='invisible'>
                                         <div className='w-28 h-32 rounded-xl shadow-md border border-gray-200 hover:bg-blue-100 hover:transition duration-200 cursor-pointer '>
                                             {/* <div className='flex justify-end -mt-5'><div className="h-8 w-8 rounded-full bg-orange-500 flex justify-center items-center text-lg text-white">1</div></div> */}
                                             <div className='w-full flex justify-center items-center '><img className='object-fit' src="/people.svg" /></div>
