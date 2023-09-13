d1f10406f639cf29999049019e1149bbb8cd5b22
AhaanRipik <ahaan@ripik.in>
2023-09-13 13:36:54 +0530

    Chat typing bubble

--- src/components/Main/Typewriter.jsx
+++ src/components/Main/Typewriter.jsx
@@ -5,7 +5,6 @@ const Typewriter = ({ text, delay }) => {
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
 
-
     useEffect(() => {
         if (currentIndex < text.length) {
           const timeout = setTimeout(() => {
