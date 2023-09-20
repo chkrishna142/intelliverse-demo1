cc292e21b9f69859c2622efd4b9f969faef3baa5
AhaanRipik <ahaan@ripik.in>
2023-09-20 14:01:58 +0530

    Route updates --expert

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
                     <Route path="/community" element={<Home state={"community"} />} />
                     <Route path="/community/advisor" element={<AiAdvisor />} />
                     <Route path="/community/askanexpert" element={<AskAnExpert />} />
-                    <Route path="/community/expert" element={<Expert />} />
+
                     {/* Client Select Page */}
                     <Route path="/client_select" element={<ClientSelect />} />
                     {/* Optimus Pages */}
@@ -95,7 +95,10 @@ function App() {
               </div>
             </>
           ) : (
-            <Login />
+            <Routes>
+              <Route path="*" element={<Login />} />
+              <Route path="/community/expert" element={<Expert />} />
+            </Routes>
           )}
         </div>
       </NavContext.Provider>
