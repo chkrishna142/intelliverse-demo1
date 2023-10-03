09b95959e8f7035d082dac3591e38821c53b24a4
GitanshBha <gitansh@ripik.in>
2023-10-03 19:03:55 +0530

    kiln changes and fixes

--- src/components/QualityTracking/Tabs/Alerts.jsx
+++ src/components/QualityTracking/Tabs/Alerts.jsx
@@ -304,17 +304,17 @@ const Alerts = ({ plantId, cameraId, disable, plantCamMap }) => {
                       key={index}
                       className="!text-sm !text-[#3E3C42] !font-medium even:bg-[#FAFAFA] odd:bg-white"
                     >
-                      <Td className="cursor-pointer">
+                      <Td className="">
                         {String(item["idx"]).padStart(2, "0")}
                       </Td>
-                      <Td className="cursor-pointer">{item.plantName}</Td>
-                      <Td className="cursor-pointer">{item.cameraId}</Td>
-                      <Td className="cursor-pointer">
+                      <Td className="">{item.plantName}</Td>
+                      <Td className="">{item.cameraId}</Td>
+                      <Td className="">
                         {new Date(item.timestamp).toLocaleDateString() +
                           " " +
                           new Date(item.timestamp).toLocaleTimeString()}
                       </Td>
-                      <Td className="cursor-pointer">
+                      <Td className="">
                         {/* <Flex gap="1rem" align="center">
                           <div className="flex flex-col justify-center items-center">
                             <Image
@@ -327,10 +327,10 @@ const Alerts = ({ plantId, cameraId, disable, plantCamMap }) => {
                         </Flex>{" "} */}
                         {item.type}
                       </Td>
-                      <Td className="cursor-pointer">
+                      <Td className="">
                         {item.mgw}
                       </Td>
-                      <Td className="cursor-pointer">
+                      <Td className="">
                         {String(item.avgGapWidths)}
                       </Td>
                       <Td>
