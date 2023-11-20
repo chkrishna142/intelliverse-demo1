6ada313159c44816cced3ebc75ee88e8390ef813
sharunnd <sharun@ripik.ai>
2023-11-20 12:29:32 +0530

    table date and time format

--- src/components/Admin/TransactionHistory/TransactionHistory.jsx
+++ src/components/Admin/TransactionHistory/TransactionHistory.jsx
@@ -5,90 +5,90 @@ import TransactionHistoryTable from "./TransactionHistoryTable";
 import Pagination from "./Pagination";
 import { NavLink, useNavigate } from "react-router-dom";
 
-
 const TransactionHistory = () => {
   const [alertsChanging, setAlertsChanging] = useState(false);
-  const [dummyData,setDummyData] = useState([
+  const [dummyData, setDummyData] = useState([
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "12:30",
       description: "Purchase",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "16 Nov 23",
+      date: "16 Nov '23",
       time: "01:30",
       description: "Transfer",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "12:30",
       description: "AI Advisor usage",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "17:00",
       description: "Purchase",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "16 Nov 23",
+      date: "16 Nov '23",
       time: "16:30",
       description: "Transfer",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "12:30",
       description: "AI Advisor usage",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "16 Nov 23",
+      date: "16 Nov '23",
       time: "01:30",
       description: "Transfer",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "12:30",
       description: "AI Advisor usage",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "17:00",
       description: "Purchase",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "16 Nov 23",
+      date: "16 Nov '23",
       time: "16:30",
       description: "Transfer",
       tokens: "20",
       balance: "20",
     },
     {
-      date: "15 Nov 23",
+      date: "15 Nov '23",
       time: "12:30",
       description: "AI Advisor usage",
       tokens: "20",
       balance: "20",
-    }
+    },
   ]);
   const [displayData, setDisplayData] = useState([]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
   const [fromTime, setFromTime] = useState(
     new Date(new Date().getTime() - 24 * 60 * 60 * 1000 + 5.5 * 60 * 60 * 1000)
       .toISOString()
@@ -109,13 +109,17 @@ const TransactionHistory = () => {
     window.history.back();
   };
   // navigating to addtokens page
-  const handleToken = () =>{
-    navigate("/community/advisor/buycredits")
+  const handleToken = () => {
+    navigate("/community/advisor/buycredits");
   };
-  
+
   useEffect(() => {
     handleClick();
   }, []);
+
+  const handleAdvisorHistory = () => {
+    navigate("/community/advisor/history");
+  };
 
   return (
     <div className="mt-[3vh]">
@@ -175,11 +179,7 @@ const TransactionHistory = () => {
                   <p className="text-[#3E3C42] font-semibold text-[14px]">
                     2000
                   </p>
-                  <img
-                    src="/token.svg"
-                    className="w-full h-full"
-                    alt="token"
-                  />
+                  <img src="/token.svg" className="w-full h-full" alt="token" />
                 </div>
               </div>
               <div onClick={handleToken}>
@@ -203,9 +203,12 @@ const TransactionHistory = () => {
           <TransactionHistoryTable dummyData={displayData} />
 
           <div className="flex items-center gap-4 mb-[10px] ">
-            <div className="bg-[#447ED4] text-[#FFFFFF] rounded-lg text-[16px] font-semibold">
+            <div
+              className="bg-[#447ED4] text-[#FFFFFF] rounded-lg text-[16px] font-semibold"
+              onClick={handleAdvisorHistory}
+            >
               <button className="px-[16px] py-[10px]">
-                <NavLink to="/community/advisor/history">AI Advisor question history</NavLink>
+                AI Advisor question history
               </button>
             </div>
             {/* <div className="bg-[#447ED4] text-[#FFFFFF] rounded-lg text-[16px] font-semibold">
--- src/components/Admin/TransactionHistory/TransactionHistoryTable.jsx
+++ src/components/Admin/TransactionHistory/TransactionHistoryTable.jsx
@@ -11,14 +11,14 @@ import {
 const TransactionHistoryTable = ({ dummyData }) => {
   return (
     <div className="w-[55vw] border border-gray-100">
-      <TableContainer className="w-[55vw] !text-center  border rounded-md bg-white">
+      <TableContainer className="!text-center  border rounded-md bg-white overflow-x-auto">
         <Table variant="simple">
           <Thead className="bg-[#DDEEFF] !text-[#79767D] !text-xs !top-0">
             <Tr>
               <Th className="!text-[#79767D] !text-center !text-sm !font-normal">
-                TRANSACTION TIME
+                DATE/TIME
               </Th>
-              <Th className="!text-[#79767D] !text-center !text-sm !font-normal">
+              <Th className="!text-[#79767D] !text-center !text-sm !font-normal"> 
                 DESCRIPTION
               </Th>
               <Th className="!text-[#79767D] !text-center !text-sm !font-normal">
@@ -35,7 +35,7 @@ const TransactionHistoryTable = ({ dummyData }) => {
                 return (
                   <Tr>
                     <Td className="!text-center !text-normal !text-[#3E3C42] !text-[14px] flex items-center justify-center gap-2" style={{fontWeight:400}}>
-                      <p className=" pl-2 pr-2">{item.date}</p>
+                      <p className="pl-2 pr-2">{item.date}</p>
                       <p className="px-4"> {item.time}</p>
                     </Td>
                     <Td className="!text-center !text-normal !text-[#3E3C42] !text-[14px]" style={{fontWeight:500}}>
