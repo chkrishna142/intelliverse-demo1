43de23898898a68485d89bd8e2d6812db2b794af
GitanshBha <gitansh@ripik.in>
2023-12-15 23:57:37 +0530

    convert to tablist, and fixes self service

--- src/components/SelfService/pages/ViewForm.jsx
+++ src/components/SelfService/pages/ViewForm.jsx
@@ -3,30 +3,31 @@ import UploadDetails from "../Components/UploadDetails";
 import ProjectDetails from "../Components/ProjectDetails";
 
 const ViewForm = ({ userState }) => {
-  // let val = Object.keys(userState?.detail[0]?.dataset || {})[0];
   return (
     <div className="flex flex-col gap-3 h-full w-full">
       <ProjectDetails userData={userState} viewMode={true} />
       <UseCaseDetail userData={userState} viewMode={true} show={true} />
       <UploadDetails userData={userState} viewMode={true} show={true} />
-      {/* <div className="flex flex-col gap-3 p-6">
+      <div className="flex flex-col gap-3 p-6">
+        <p className="text-[#3E3C42] text-xl font-medium">Annotated data</p>
         <div className="w-full h-fit max-h-full overflow-y-auto grid-cols-1 grid min-[430px]:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 gap-5">
-          {userState?.annotations[val]?.map((item, idx) => {
-            return (
-              <div className="w-full h-auto bg-black relative rounded flex items-center justify-center">
-                <img
-                  src={userState?.detail[0]?.dataset[val][item.fileId]}
-                  alt="image"
-                  className="w-full rounded"
-                />
-                <p className="flex items-center gap-1 p-1 rounded-full bg-black absolute top-2 right-2 text-white font-medium text-sm bg-opacity-50">
-                  {item.label}
-                </p>
-              </div>
-            );
-          })}
+          {userState.annotatedData != null &&
+            userState?.annotatedData.map((item, idx) => {
+              return (
+                <div className="w-full h-auto bg-black relative rounded flex items-center justify-center">
+                  <img
+                    src={userState?.uploadedFiles[item.fieldId]}
+                    alt="image"
+                    className="w-full rounded"
+                  />
+                  <p className="flex items-center gap-1 p-1 rounded-full bg-black absolute top-2 right-2 text-white font-medium text-sm bg-opacity-50">
+                    {item.label}
+                  </p>
+                </div>
+              );
+            })}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
